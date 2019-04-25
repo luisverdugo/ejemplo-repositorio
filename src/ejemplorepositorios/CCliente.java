a5b5b925d90c706601e908292d2ad387bc0c6abc
dam07 <dam07@PC07.aula1.com>
2019-04-25 11:12:34 +0200

    Primera modificación ejercicio 5  3º parte. creamos nueva clase CCliente

---
+++ src/ejemplorepositorios/CCliente.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplorepositorios;
+
+/**
+ *
+ * @author dam07
+ */
+public class CCliente {
+    private final String nombre;
+    private final String apellido1;
+    private final String apellido2;
+
+public CCliente (String nom, String ape1, String ape2)
+    {
+        nombre = nom;
+        apellido1= ape1;
+        apellido2= ape2;
+  
+ }
+}
